6335cea87663d9a2d34555ba6da66a19fbefe59b
Jorge Soares <547492+jsoares@users.noreply.github.com>
2024-01-15 10:46:25 +0100

    Check params

--- src/lib.rs
+++ src/lib.rs
@@ -17,6 +17,11 @@ use scilib::math::bessel;
 /// * A `f64` representing the PMF of the Skellam distribution at `k`.
 ///
 pub fn skellam_pmf(k: i32, mu1: f64, mu2: f64) -> f64 {
+    // Return NaN if parameters outside range
+    if mu1.is_nan() || mu1 <= 0.0 || mu2.is_nan() || mu2 <= 0.0 {
+        return f64::NAN;
+    }
+
     // Parameterise and compute the Modified Bessel function of the first kind
     let nu = k as f64;
     let z = Complex64::new(2.0 * (mu1 * mu2).sqrt(), 0.0);
